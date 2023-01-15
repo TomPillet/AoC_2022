1a472d9d0976186db86e6ec8c7b567ae6e9c2c25
TPI <tom963741@gmail.com>
2023-01-15 07:02:21 +0100

    part one solution

--- Day_02/script.js
+++ Day_02/script.js
@@ -1,23 +1,44 @@
 var fs = require('fs');
-const input = fs.readFileSync('/home/tompillet/Documents/0 - Coding/AOC/Day_01/input.txt', 'utf-8');
+const input = fs.readFileSync('/home/tompillet/Documents/0 - Coding/AOC/Day_02/input.txt', 'utf-8');
 
-// ROCK = A, X
-// SCISSORS = B, Y
-// PAPER = C, Z
+// ROCK = A, X      - it equals to 1 point
+// SCISSORS = B, Y  - it equals to 2 points
+// PAPER = C, Z     - it equals to 3 points
+// A win equals to 6 points, a draw equals to 3, and a loose equals to 0
 
 // PART ONE
 const rules = {
-    'A X' : 'D',
-    'A Y' : 'L',
-    'A Z' : 'W',
+    scoreTransform: {
+        
+        'X' : 1,
+        'Y' : 2,
+        'Z' : 3,
 
-    'B X' : 'W',
-    'B Y' : 'D',
-    'B Z' : 'L',
+    },
 
-    'C X' : 'L',
-    'C Y' : 'W',
-    'C Z' : 'D',
+    roundResult: {
+
+        'A X' : 3,
+        'A Y' : 6,
+        'A Z' : 0,
+    
+        'B X' : 0,
+        'B Y' : 3,
+        'B Z' : 6,
+    
+        'C X' : 6,
+        'C Y' : 0,
+        'C Z' : 3,
+
+    }
 }
 
 const gameOne = input.split(/\n/);
+const rounds = gameOne.map(round => {
+    const roundResult = rules.roundResult[round];
+    const playScore = rules.scoreTransform[round[2]];
+    
+    return roundResult + playScore;
+});
+let gameResult = rounds.reduce((a,c) => a+c, 0);
+console.log(gameResult);
