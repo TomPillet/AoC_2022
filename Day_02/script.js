7c3ee9ee505ee7189355d0f4e96a307b4ed1fee0
TPI <tom963741@gmail.com>
2023-01-15 09:44:00 +0100

    Day_02 - Part 2 solution

--- Day_02/script.js
+++ Day_02/script.js
@@ -1,44 +1,81 @@
 var fs = require('fs');
 const input = fs.readFileSync('/home/tompillet/Documents/0 - Coding/AOC/Day_02/input.txt', 'utf-8');
 
-// ROCK = A, X      - it equals to 1 point
-// SCISSORS = B, Y  - it equals to 2 points
-// PAPER = C, Z     - it equals to 3 points
-// A win equals to 6 points, a draw equals to 3, and a loose equals to 0
+const game = input.split(/\n/);
 
-// PART ONE
-const rules = {
-    scoreTransform: {
+const scoreTransform = {
         
-        'X' : 1,
-        'Y' : 2,
-        'Z' : 3,
+    'X' : 1,
+    'Y' : 2,
+    'Z' : 3,
 
-    },
+    'R' : 1,
+    'P' : 2,
+    'S' : 3,
 
-    roundResult: {
+};
 
-        'A X' : 3,
-        'A Y' : 6,
-        'A Z' : 0,
-    
-        'B X' : 0,
-        'B Y' : 3,
-        'B Z' : 6,
-    
-        'C X' : 6,
-        'C Y' : 0,
-        'C Z' : 3,
-
-    }
+function getGameResult(gameInput, rules) {
+    const rounds = gameInput.map(round => {
+        const roundResult = rules[round];
+        const playScore = scoreTransform[round[2]];
+        
+        return roundResult + playScore;
+    });
+    return rounds.reduce((a,c) => a+c, 0);
 }
 
-const gameOne = input.split(/\n/);
-const rounds = gameOne.map(round => {
-    const roundResult = rules.roundResult[round];
-    const playScore = rules.scoreTransform[round[2]];
+// PART ONE
+const rulesOne = {
+
+    'A X' : 3,
+    'A Y' : 6,
+    'A Z' : 0,
+
+    'B X' : 0,
+    'B Y' : 3,
+    'B Z' : 6,
+
+    'C X' : 6,
+    'C Y' : 0,
+    'C Z' : 3,
+
+};
+
+console.log(getGameResult(game, rulesOne));
+
+
+// PART TWO
+const strategyGuide = {
+
+    'A X' : 'A S',
+    'A Y' : 'A R',
+    'A Z' : 'A P',
+
+    'B X' : 'B R',
+    'B Y' : 'B P',
+    'B Z' : 'B S',
+
+    'C X' : 'C P',
+    'C Y' : 'C S',
+    'C Z' : 'C R'
+
+}
+const rulesTwo = {
+
+    'A R' : 3,
+    'A P' : 6,
+    'A S' : 0,
+
+    'B R' : 0,
+    'B P' : 3,
+    'B S' : 6,
+
+    'C R' : 6,
+    'C P' : 0,
+    'C S' : 3
     
-    return roundResult + playScore;
-});
-let gameResult = rounds.reduce((a,c) => a+c, 0);
-console.log(gameResult);
+}
+
+const formatedGame = game.map(round => strategyGuide[round]);
+console.log(getGameResult(formatedGame, rulesTwo));
