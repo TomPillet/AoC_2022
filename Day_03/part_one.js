ba41cf6757484a08c9d9652a1faacf1a46774ff6
TPI <tom963741@gmail.com>
2023-01-24 22:43:52 +0100

    change variables name

--- Day_03/part_one.js
+++ Day_03/part_one.js
@@ -3,16 +3,15 @@ const input = fs.readFileSync(__dirname+'/input.txt', 'utf-8').trim().split(/\n/
 
 // TODO : vérification si la longueur du sample est paire ou impaire
 
-const commonItems = input.map(sample => {
-    let halfLenght = sample.length / 2; 
-    let halfSample = sample.substring(halfLenght);
-
+const commonItems = input.map(rucksack => {
+    let halfLenght = rucksack.length / 2; 
+    let halfRucksack = rucksack.substring(halfLenght);
     let commonItem = '';
 
-    for (let i=0; i < halfLenght; i++) {
-        if (halfSample.includes(sample[i])) {
-            commonItem = sample[i];
-            i = halfLenght;
+    for (let charIndex = 0; charIndex < halfLenght; charIndex++) {
+        if (halfRucksack.includes(rucksack[i])) {
+            commonItem = rucksack[charIndex];
+            charIndex = halfLenght;
         }
     }  
 
@@ -20,8 +19,7 @@ const commonItems = input.map(sample => {
 });
 
 const formatedItems = commonItems.map(item => {
-    let priority = 0;
-    if (item === item.toUpperCase()) priority = 26;
+    let priority = (item === item.toUpperCase()) ? 26 : 0;
 
     item = item.toUpperCase();
     priority += parseInt(item.replace(/[A-Z]/g, i => i.toUpperCase().charCodeAt(0) - 64));
@@ -29,6 +27,4 @@ const formatedItems = commonItems.map(item => {
     return priority;
 })
 
-const prioritySum = formatedItems.reduce((a,c) => a+c, 0)
-
-console.log(prioritySum);
+console.log(formatedItems.reduce((a,c) => a+c, 0));
--- Day_03/part_two.js
+++ Day_03/part_two.js
@@ -2,32 +2,29 @@ const fs = require('fs');
 const input = fs.readFileSync(__dirname+'/input.txt', 'utf-8').trim().split(/\n/);
 const commonItems = [];
 
-let sampleIndex = 0;
-while (sampleIndex < input.length) {
-    const sample = input[sampleIndex];
+let rucksackIndex = 0;
+while (rucksackIndex < input.length) {
+    const firstGroupRucksack = input[rucksackIndex];
 
-    for (let charIndex = 0; charIndex < sample.length; charIndex++) {
-        const char = sample.charAt(charIndex);
+    for (let charIndex = 0; charIndex < firstGroupRucksack.length; charIndex++) {
+        const char = firstGroupRucksack.charAt(charIndex);
 
-        if(input[sampleIndex+1].includes(char) && input[sampleIndex+2].includes(char)) {
+        if(input[rucksackIndex+1].includes(char) && input[rucksackIndex+2].includes(char)) {
             commonItems.push(char);
-            charIndex = sample.length;
+            charIndex = firstGroupRucksack.length;
         }
     }
 
-    sampleIndex += 3;
+    rucksackIndex += 3;
 }
 
 const formatedItems = commonItems.map(item => {
-    let priority = 0;
-    if (item === item.toUpperCase()) priority = 26;
+    let priority = (item === item.toUpperCase()) ? 26 : 0;
 
     item = item.toUpperCase();
     priority += parseInt(item.replace(/[A-Z]/g, i => i.toUpperCase().charCodeAt(0) - 64));
 
     return priority;
-})
+});
 
-const prioritySum = formatedItems.reduce((a,c) => a+c, 0)
-
-console.log(prioritySum);
+console.log(formatedItems.reduce((a,c) => a+c, 0));
