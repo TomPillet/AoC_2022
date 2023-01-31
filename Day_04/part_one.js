96c2143122695bc4c4a398c11ce577953e4f68cc
Tom PILLET <tom963741@gmail.com>
2023-01-31 08:41:18 +0100

    day04 - first half completed

--- Day_04/part_one.js
+++ Day_04/part_one.js
@@ -1,40 +1,27 @@
 const fs = require('fs');
-const test = fs.readFileSync(__dirname+'/test.txt', 'utf-8').trim().split(/\n/);
-// const input = fs.readFileSync(__dirname+'/input.txt', 'utf-8').trim().split(/\n/);
+// const test = fs.readFileSync(__dirname+'/test.txt', 'utf-8').trim().split(/\n/);
+const input = fs.readFileSync(__dirname+'/input.txt', 'utf-8').trim().split(/\n/);
 
+console.log(parseData(input));
 
-console.log(test);
-console.log(parseData(test));
-
-function parseData(input) {
-    const formatedData = input.map(x => x.split(','));
+function parseData(data) {
+    const formatedData = data.map(x => x.split(','));
     let countCommonRanges = 0;
 
     for (let i = 0; i < formatedData.length; i++) {
-        const minSectionFirstElf = parseInt(formatedData[i][0].charAt(0));
-        const maxSectionFirstElf = parseInt(formatedData[i][0].charAt(2));
+        const formatedSectionFirstElf = formatedData[i][0].split('-');
+        const minSectionFirstElf = parseInt(formatedSectionFirstElf[0]);
+        const maxSectionFirstElf = parseInt(formatedSectionFirstElf[1]);
         
-        const minSectionSecondElf = parseInt(formatedData[i][1].charAt(0));
-        const maxSectionSecondElf = formatedData[i][1].charAt(2);
+        const formatedSectionSecondElf = formatedData[i][1].split('-');
+        const minSectionSecondElf = parseInt(formatedSectionSecondElf[0]);
+        const maxSectionSecondElf = parseInt(formatedSectionSecondElf[1]);
     
-        if (maxSectionFirstElf <= minSectionSecondElf || minSectionFirstElf >= maxSectionFirstElf) {
-            i = formatedData.length;
-        } else {
 
-            let minLessThanMax = true;
-            while(minLessThanMax && minSectionFirstElf < maxSectionFirstElf) {
-
-                if (minSectionFirstElf === maxSectionFirstElf) {
-                    minLessThanMax = false;
-                }
-
-                minSectionFirstElf++;
-            }
-
-            if (minLessThanMax) {
-                countCommonRanges++;
-            }
-
+        if ((minSectionFirstElf <= minSectionSecondElf && maxSectionFirstElf >= maxSectionSecondElf)
+            || (minSectionFirstElf >= minSectionSecondElf && maxSectionFirstElf <= maxSectionSecondElf))
+        {
+            countCommonRanges++;
         }
     }
 
